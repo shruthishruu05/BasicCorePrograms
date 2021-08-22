480d0f4a4cbc3e5361db673144902fed10f4e31e
shruthishruu05 <shruthi.m@perfios.com>
2021-08-22 13:36:36 +0530

    ADDED: QuotientAndRemainder program

---
+++ src/com/bridgelabz/BasicCorePrograms/QuotientAndRemainder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.BasicCorePrograms;
+
+public class QuotientAndRemainder {
+	public static void main(String args[]) 
+	{
+		int number=168,divisor=3;
+		System.out.println("Quotient: "+number/divisor);
+		System.out.println("Remainder:" +number%divisor);
+
+	}
+}
+
+
+
